de0d470ed8791632fe740cbf33420eddff3ada67
radagon828 <looky290@gmail.com>
2021-02-13 03:07:30 -0600

    added run.java

--- src/main/java/model/Run.java
+++ src/main/java/model/Run.java
@@ -6,4 +6,44 @@ public class Run {
     private double time;
     private String platform;
     private String date;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
